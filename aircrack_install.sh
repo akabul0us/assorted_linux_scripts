d663140377bab3a6c8955707bb2a30f7c3efb025
Akab Ulous <jeremywokka@proton.me>
2025-09-19 18:45:08 -0600

    added support for arch based distros and (untested) openbsd and freebsd

--- aircrack_install.sh
+++ aircrack_install.sh
@@ -3,27 +3,59 @@ if [ "$EUID" -ne 0 ]; then
     printf "Run it as root\n"
     exit 1
 fi
-if [ ! -e "/etc/debian_version" ]; then
-	echo "This script uses Debian package manager apt to install dependencies"
-	exit 1
+opsys="$(uname -s)"
+if [ "$opsys" == "Linux" ]; then
+    make_command="make"
+    cat /etc/*release | grep -i debian > /dev/null
+    is_debian="$?"
+    if [ "$is_debian" -eq 0 ]; then
+        distro="debian"
+    else
+       cat /etc/*release | grep -i arch > /dev/null
+       is_arch="$?"
+       if [ "$is_arch" -eq 0 ]; then
+            distro="arch"
+        else
+            echo "Unsupported distribution detected, quitting"
+            exit 1
+        fi
+    fi
+elif [ "$opsys" == "OpenBSD" ]; then
+    make_command="gmake"
+    distro="openbsd"
+elif [ "$opsys" == "FreeBSD" ]; then
+    make_command="gmake"
+    distro="freebsd"
+else
+    echo "Unsupported OS detected"
+    echo "This script currently supports GNU/Linux distributions based on Debian or Arch, FreeBSD, and OpenBSD."
+    exit 1
 fi
-dependencies="libstdc++-14-dev build-essential autoconf automake libtool shtool libssl-dev usbutils libnl-3-dev libnl-genl-3-dev make pkg-config libpcre2-dev libsqlite3-dev hwloc libhwloc-plugins libcmocka0 libcmocka-dev curl tar"
-apt-get install $dependencies -y
-res="$?"
-if [ "$res" -ne 0 ]; then
-	echo "Attempting one by one"
-	for d in $dependencies; do
-	    apt-get install -y $d
+if [ "$distro" == "debian" ]; then
+    dependencies="libstdc++-14-dev build-essential autoconf automake libtool shtool libssl-dev usbutils libnl-3-dev libnl-genl-3-dev make pkg-config libpcre2-dev libsqlite3-dev hwloc libhwloc-plugins libcmocka0 libcmocka-dev curl tar"
+    apt-get install $dependencies -y
+    res="$?"
+    if [ "$res" -ne 0 ]; then
+	    echo "Attempting one by one"
+    	for d in $dependencies; do
+	        apt-get install -y $d
     	done
+    fi
+elif [ "$distro" == "arch" ]; then
+    pacman -Sy base-devel libnl openssl ethtool util-linux zlib libpcap sqlite pcre2 hwloc cmocka hostapd wpa_supplicant tcpdump screen iw usbutils pciutils curl tar
+elif [ "$distro" == "openbsd" ]; then
+    pkg_add pkgconf shtool libtool gcc automake autoconf pcre sqlite3 openssl gmake cmocka curl tar
+elif [ "$distro" == "freebsd" ]; then
+    pkg install pkgconf shtool libtool gcc9 automake autoconf pcre sqlite3 openssl gmake hwloc cmocka curl tar
 fi
 cd $HOME
 if [ ! -d "$HOME/aircrack-ng-1.7" ]; then
 	curl -fsSL https://download.aircrack-ng.org/aircrack-ng-1.7.tar.gz | tar xzvf -
 fi
 cd $HOME/aircrack-ng-1.7
-make clean
+${make_command} clean
 autoreconf -i
 ./configure --with-experimental --with-ext-scripts --prefix=/usr
-make
-make install
+${make_command} -j$(nproc --all)
+${make_command} install
 ldconfig
