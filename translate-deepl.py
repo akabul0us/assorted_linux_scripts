5684a04336de3c3e9ad2cfe9eb557963150a6d01
akabul0us <jeremywokka@proton.me>
2025-12-19 13:17:06 -0600

    added language selection functionality to translator

--- translate-deepl.py
+++ translate-deepl.py
@@ -24,16 +24,25 @@ except OSError:
             print("...wherever that is on your system")
     platform_check
     sys.exit(1)
+LANGUAGE_OPTIONS={"AR", "BG", "CS", "DA", "DE", "EL", "EN-GB", "EN-US", "ES", "ES-419", "ET", "FI", "FR", "HE", "HU", "ID", "IT", "JA", "KO", "LT", "LV", "NB", "NL", "PL", "PT-BR", "PT-PT", "RO", "RU", "SK", "SL", "SV", "TH", "TR", "UK", "VI", "ZH", "ZH-HAN"}
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help="text file to translate")
+parser.add_argument("language", type=str, help="target language - code like ES or EN-US")
 args = parser.parse_args()
+if args.language not in LANGUAGE_OPTIONS:
+    print(f"Error: '{args.language}' is not a recognized language.")
+    print('Recognized languages: AR (Arabic) BG (Bulgarian) CS (Czech) DA (Danish) DE (German) EL (Greek) EN-GB (British English) EN-US (American English) ES (Spanish) ES-419 (LatAm Spanish) ET (Estonian) FI (Finnish) FR (French) HE (Hebrew) HU (Hungarian) ID (Indonesian) IT (Italian) JA (Japanese) KO (Korean) LT (Lithuanian) LV (Latvian) NB (Norwegian) NL (Dutch) PL (Polish) PT-BR (Brazilian Portuguese) PT-PT (European Portuguese) RO (Romanian) RU (Russian) SK (Slovak) SL (Slovenian) SV (Swedish) TH (Thai) TR (Turkish) UK (Ukrainian) VI (Vietnamese) ZH (Simplified Chinese) ZH-HANS (Simplified Chinese) ZH-HANT (Traditional Chinese)')
+    sys.exit(1)
+else:
+    language = args.language
 text_file = (args.file)
 with open(text_file, 'r') as file:
     contents = file.read()
 #make sure the authkey is passed as a string and not an object
 auth_key = str(deepl_auth_key)
 translator = deepl.Translator(auth_key)
-result = translator.translate_text(contents, target_lang="ES")
+lang = str(language)
+result = translator.translate_text(contents, target_lang="%s" % language)
 print(result.text)
 deepl_client = deepl.DeepLClient(auth_key)
 usage = deepl_client.get_usage()
