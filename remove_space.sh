cc1011e971582fe3668a0f3cfe3545bfbf29e194
akabul0us <jeremywokka@proton.me>
2026-01-09 00:18:56 -0600

    fixed the remove space if/fis`

--- remove_space.sh
+++ remove_space.sh
@@ -1,25 +1,73 @@
 #!/usr/bin/env bash
-green='\033[0;32m'
-pink='\033[0;35m'
-teal='\033[0;36m'
-yellow='\033[0;33m'
-clear_color='\033[0m'
-printf "Renaming all files with spaces or parentheses in ${green}$(pwd)${clear_color}"
-for run in {1..3}; do
-  printf "."
-  sleep 1
+print_help() {
+	printf "Usage: $0" '[-y] [-h] [-d DIRECTORY]' "\n"
+	printf "-y: skip confirmation dialog\n"
+	printf "-h: print this help\n"
+	printf "-d DIRECTORY: specify which directory to rename files in\n"
+	printf "(if no directory is given, default is the present directory\n"
+	exit 1
+}
+optflag=0
+while getopts 'yhd:' option; do
+	case $option in 
+		y) 
+			optflag="1"
+			;;
+		d)
+			directory="$OPTARG"
+			;;
+		h)
+			print_help
+			;;
+	esac
 done
-printf "\n"
-for file in *; do 
+if [ -z "$directory" ]; then
+	directory="$(pwd)"
+fi
+echo "Finding files with spaces and/or parentheses in $directory..."
+for file in $directory/*; do 
 	if [[ "$file" == *" "* ]]; then
 		new_file="${file// /_}"
-		printf "Renaming ${pink}$file${clear_color} to ${teal}$new_file${clear_color}\n"
-		mv "$file" "$new_file"
-	fi
-	if [[ "$file" == *\(* ]] || [[ "$file" == *\)* ]]; then
-		new_file="${file//\)/}"
-                newest_file="${new_file//\(/}"
-		printf "Renaming ${yellow}$file${clear_color}to ${teal}$newest_file${clear_color}\n"
-                mv "$file" "$newest_file" || mv "$new_file" "$newest_file"
+		if [ "$optflag" -eq 1 ]; then
+			echo "Renaming $file to $new_file"
+			mv "$file" "$new_file"
+			if [[ "$file" == *\(* ]] || [[ "$file" == *\)* ]]; then
+				new_file="${file//\)/}"
+                		newest_file="${new_file//\(/}"
+			fi
+			echo "Renaming $file to $newest_file"
+                	mv "$file" "$newest_file" 2>/dev/null
+			if [ "$?" -ne 0 ]; then
+				mv $new_file $newest_file
+			fi
+		else
+			read -p "Rename $file to $new_file? (Y/n)" conf
+			case $conf in
+				[Nn])
+					echo "Skipping $file"
+					;;
+				*)
+					echo "Renaming $file to $new_file"
+					mv "$file" "$new_file"
+					;;
+			esac
+			if [[ "$file" == *\(* ]] || [[ "$file" == *\)* ]]; then
+				new_file="${file//\)/}"
+                		newest_file="${new_file//\(/}"
+				read -p "Rename $file to $newest_file? (Y/n)" parenconf
+					case $parenconf in
+						[Nn])
+							echo "Skipping $file"
+							;;
+						*)
+							echo "Renaming $file to $newest_file"
+							mv "$file" "$newest_file" 2>/dev/null
+							if [ "$?" -ne 0 ]; then
+								mv $new_file $newest_file
+							fi
+							;;
+					esac
+			fi
         fi
+fi
 done
